f3b003a0630472a11ff18c8a4a688ae0d91bbc14
Gabriela Pivetta <gpivetta99@gmail.com>
2021-07-05 21:32:21 -0400

    new vue cli project

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
+        </ul>
+    </section>
+</template>
+
+<script>
+//export default instead of const app because it will be imported on another fila (main.js) and we're saying everything in here will be sent there as an object.
+export default {
+    data (){
+        return {
+            friends: [
+                {
+                    if: 'manuel',
+                    name: 'Manuel Soares',
+                    phone: '94561 8456',
+                    email: 'manuel@localhost.com'
+                },
+                {
+                    if: 'julia',
+                    name: 'Julia Mendes',
+                    phone: '98989 1234',
+                    email: 'julia@localhost.com'
+                }
+            ]
+        };
+    }
+}
+</script>
---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">Show Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone: </strong>{{ friend.phone }}</li>
+            <li><strong>Email: </strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                    if: 'manuel',
+                    name: 'Manuel Soares',
+                    phone: '94561 8456',
+                    email: 'manuel@localhost.com'
+            },
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+}
+</script>
