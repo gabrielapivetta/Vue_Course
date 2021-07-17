bd937689c99a1b3f6e27ffe6eb0e10cc4a85510d
Gabriela Pivetta <gpivetta99@gmail.com>
2021-07-16 21:10:45 -0300

    slots

---
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!-- <TheHeader/> also works -->
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <!-- #default = v-slot:dafault and subs the template tag-->
+    <course-goals #default="slotProps">
+      <h2>{{slotProps.goalItem}}</h2>
+      <p>{{slotProps['another-prop']}}</p>
+    </course-goals>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
+
+export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+    CourseGoals
+    //or 'the-header': TheHeader
+    //or TheHeader: TheHeader
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <!-- scoped slots work for  -->
+        <li v-for="goal in goals" :key="goal">
+            <slot :goalItem="goal" another-prop="..."></slot>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            goals: ['Finish the course', 'Learn Vue']
+        };       
+    },
+};
+</script>
